9d0ea53d815cdd716eb6f3d5fdee953125332aa9
Anais <dganais@gmail.com>
2018-04-04 16:56:44 -0500

    integrated model

---
+++ Integrate_Images_Anais/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import Card_Predictor
+import json
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictions')
+def predictions():
+    predictions = Card_Predictor.prediction_tuples()
+    print(predictions)
+    return jsonify(predictions)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
