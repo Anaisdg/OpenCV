923282ec3e805a9677d2e8e51608b22a9178a084
Anais <dganais@gmail.com>
2018-04-06 14:03:42 -0500

    front end

--- Integrate_Images_Anais/Card_Predictor.py
+++ Integrate_Images_Anais/Card_Predictor.py
@@ -7,6 +7,9 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 
+from os import listdir
+from PIL import Image
+
 import numpy as np
 import pandas as pd
 import os
